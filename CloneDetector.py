8eac31cb45fbc4eb6ae556c2f557a4bd30785075
Marcel Petrick <petrick@instrumentsystems.com>
2019-11-07 14:15:26 +0100

    fixed typo: unrecognizable character
    Change-Id: I05df17d969fe5104d99cb48a49dd23a6269d091c

--- CloneDetector.py
+++ CloneDetector.py
@@ -18,7 +18,7 @@ def humanReadableFormat(num, suffix='B'):
     # taken from: https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
-            return "%3.1 f%s%s" % (num, unit, suffix)
+            return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
